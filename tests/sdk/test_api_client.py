11dba4a7a1afbd94bbe707d1031a9a6019095eed
Andrew Chen <andrewmchen@berkeley.edu>
2018-04-11 21:35:49 -0700

    Fix python 3 basic auth. (#109)
    * fix
    
    * init

---
+++ tests/sdk/test_api_client.py
@@ -0,0 +1,31 @@
+# Databricks CLI
+# Copyright 2017 Databricks, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"), except
+# that the use of services to which certain application programming
+# interfaces (each, an "API") connect requires that the user first obtain
+# a license for the use of the APIs from Databricks, Inc. ("Databricks"),
+# by creating an account at www.databricks.com and agreeing to either (a)
+# the Community Edition Terms of Service, (b) the Databricks Terms of
+# Service, or (c) another written agreement between Licensee and Databricks
+# for the use of the APIs.
+#
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from databricks_cli.sdk.api_client import ApiClient
+
+
+def test_api_client_constructor():
+    """This used to throw when we converted <user>:<password> to base64 encoded string."""
+    client = ApiClient(user='apple', password='banana', host='https://databricks.com')
+    # echo -n "apple:banana" | base64
+    assert client.default_headers['Authorization'] == 'Basic YXBwbGU6YmFuYW5h'
