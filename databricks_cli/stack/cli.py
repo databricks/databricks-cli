602a8cc1f4484298ab05ddab22f0ea77a9d1522e
Andrew Linxie <andrew.linxie@databricks.com>
2018-07-19 17:06:10 -0700

    minor changes

--- databricks_cli/stack/cli.py
+++ databricks_cli/stack/cli.py
@@ -44,8 +44,7 @@ def deploy(api_client, config_path, **kwargs):
     Deploy a stack to the databricks workspace given a JSON stack configuration template.
     """
     click.echo('#' * 80)
-    click.echo('Deploying stack at: ' + config_path)
-    click.echo(kwargs)
+    click.echo('Deploying stack at: {} with options: {}'.format(config_path, kwargs))
     StackApi(api_client).deploy(config_path, **kwargs)
     click.echo('#' * 80)
 
