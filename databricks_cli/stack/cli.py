f534977d8ff642e2be3d113b9f1f73db3af7033b
Weifeng Ji <weifeng.ji@databricks.com>
2018-10-09 21:33:57 -0700

    add beta version tag to stack CLI (#177)
    * add beta tag to stack CLI
    
    * address xuan
    
    * fix config test so that it succeed uder CLICK 7.0
    
    * improving style
    
    * address xuan

--- databricks_cli/stack/cli.py
+++ databricks_cli/stack/cli.py
@@ -142,14 +142,14 @@ def download(api_client, config_path, **kwargs):
 
 
 @click.group(context_settings=CONTEXT_SETTINGS,
-             short_help='Utility to deploy and download Databricks resource stacks.')
+             short_help='[Experimental] Utility to deploy and download Databricks resource stacks.')
 @click.option('--version', '-v', is_flag=True, callback=print_version_callback,
               expose_value=False, is_eager=True, help=version)
 @debug_option
 @profile_option
 def stack_group():
     """
-    Utility to deploy and download Databricks resource stacks.
+    [Experimental] Utility to deploy and download Databricks resource stacks.
     """
     pass
 
