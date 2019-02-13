34b1749488c8e6bbcaeadad7ca3df89dfde12488
Andrew Chen <andrewmchen@berkeley.edu>
2019-02-13 14:21:01 -0800

    Bump version to 0.8.4 (#211)

--- databricks_cli/version.py
+++ databricks_cli/version.py
@@ -21,7 +21,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-version = '0.8.3' #  NOQA
+version = '0.8.4' #  NOQA
 
 
 def print_version_callback(ctx, param, value): #  NOQA
