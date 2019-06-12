c399ed629c7082cb2ea86c65b24b1941d92111e7
tomasatdatabricks <tomas.nykodym@databricks.com>
2019-06-12 11:17:03 -0700

    lint

--- databricks_cli/configure/provider.py
+++ databricks_cli/configure/provider.py
@@ -211,20 +211,20 @@ class SparkTaskContextConfigProvider(DatabricksConfigProvider):
             return TaskContext.get()
         except ImportError:
             return None
-    
+  
     @classmethod
     def set_ignore_insecure(cls, x):
-        from pyspark import SparkContext        
+        from pyspark import SparkContext  # pylint: disable=import-error      
         new_val = "True" if x else None
         SparkContext._active_spark_context.setLocalProperty("spark.databricks.ignoreTls", new_val)
-    
+ 
     def get_config(self):
         context = self._get_spark_task_context_or_none()
         if context is not None:
             host = context.getLocalProperty("spark.databricks.api.url")
             token = context.getLocalProperty("spark.databricks.token")
-            ignoreTls = context.getLocalProperty("spark.databricks.ignoreTls")
-            config = DatabricksConfig.from_token(host=host, token=token, insecure=ignoreTls)
+            insecure = context.getLocalProperty("spark.databricks.ignoreTls")
+            config = DatabricksConfig.from_token(host=host, token=token, insecure=insecure)
             if config.is_valid:
                 return config
         return None
