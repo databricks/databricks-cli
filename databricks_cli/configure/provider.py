873a2a8a1f1e68e83a26952009fd3da1da2c2024
tomasatdatabricks <tomas.nykodym@databricks.com>
2019-06-13 13:54:33 -0700

    set insecure classmethod -> staticmethod

--- databricks_cli/configure/provider.py
+++ databricks_cli/configure/provider.py
@@ -212,8 +212,8 @@ class SparkTaskContextConfigProvider(DatabricksConfigProvider):
         except ImportError:
             return None
 
-    @classmethod
-    def set_insecure(cls, x):
+    @staticmethod
+    def set_insecure(x):
         from pyspark import SparkContext  # pylint: disable=import-error      
         new_val = "True" if x else None
         SparkContext._active_spark_context.setLocalProperty("spark.databricks.ignoreTls", new_val)
