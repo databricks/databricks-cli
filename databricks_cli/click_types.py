ba77943f5dc6d509f9938043811a7e1e98e2e41d
Allen Reese <areese78@yahoo.com>
2019-07-15 16:58:21 -0700

    Remove extra import

--- databricks_cli/click_types.py
+++ databricks_cli/click_types.py
@@ -20,7 +20,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import logging
 
 import click
 from click import ParamType, Option, MissingParameter, UsageError
