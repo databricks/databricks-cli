b789ce1063c454c1b0773f89f8d5f45bd94cf146
Allen Reese <areese78@yahoo.com>
2019-07-15 16:55:22 -0700

    Remove excess logging configuration that isn't needed

--- databricks_cli/click_types.py
+++ databricks_cli/click_types.py
@@ -122,13 +122,6 @@ class ContextObject(object):
         click.echo("HTTP debugging enabled")
         http_client.HTTPConnection.debuglevel = 1
 
-        # You must initialize logging, otherwise you'll not see debug output.
-        logging.basicConfig()
-        logging.getLogger().setLevel(logging.DEBUG)
-        requests_log = logging.getLogger("requests.packages.urllib3")
-        requests_log.setLevel(logging.DEBUG)
-        requests_log.propagate = True
-
     @property
     def debug_mode(self):
         return self._debug
