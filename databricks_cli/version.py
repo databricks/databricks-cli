643563f7f9d4de2ad8cea16c4d9ce71935c1a8da
Andrew Chen <andrewmnchen@gmail.com>
2019-08-28 14:34:19 -0700

    init

--- databricks_cli/version.py
+++ databricks_cli/version.py
@@ -21,7 +21,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-version = '0.8.8.dev0' #  NOQA
+version = '0.9.0' #  NOQA
 
 
 def print_version_callback(ctx, param, value): #  NOQA
