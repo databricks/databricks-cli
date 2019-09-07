3f51f181a6d5a8a41d0f56941fde6b603351983e
David Lewis <david.lewis@databricks.com>
2019-09-06 22:28:57 -0600

    fixing print to be version agnostic

--- databricks_cli/dbfs/api.py
+++ databricks_cli/dbfs/api.py
@@ -129,7 +129,7 @@ class DbfsApi(object):
                 except:
                     if retries_left > 0:
                         time.sleep(1)
-                        print 'Error reading chunk, trying again (', retries_left, ' left)!'
+                        print('Error reading chunk, trying again (' + str(retries_left) + ' left)!')
                         retries_left = retries_left - 1
                     else:
                         raise
