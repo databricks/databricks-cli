207f9120815cc4f34c44798a6f8cde75d446578e
Bogdan Ghita <bogdan.ghita@databricks.com>
2020-09-23 17:28:38 +0200

    Bump version to 0.12.1.dev0

--- databricks_cli/version.py
+++ databricks_cli/version.py
@@ -21,7 +21,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-version = '0.12.0' #  NOQA
+version = '0.12.1.dev0' #  NOQA
 
 
 def print_version_callback(ctx, param, value): #  NOQA
