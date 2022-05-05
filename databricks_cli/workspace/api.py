c695b8db92917f18bf3d6a6a22acc88fe743a3e2
Ege E <ege.elgun@databricks.com>
2022-05-05 14:28:11 +0200

    Sample - Fix exports from Workspace to Windows FS (#448)

--- databricks_cli/workspace/api.py
+++ databricks_cli/workspace/api.py
@@ -22,6 +22,7 @@
 # limitations under the License.
 
 import os
+import re
 from base64 import b64encode, b64decode
 
 import click
@@ -35,6 +36,7 @@ DIRECTORY = 'DIRECTORY'
 NOTEBOOK = 'NOTEBOOK'
 LIBRARY = 'LIBRARY'
 REPO = 'REPO'
+LOCAL_OS_COMPATIBLE_PATH_REGEX = r'[^\w\-_\. ]' + re.escape(os.sep)
 
 
 class WorkspaceFileInfo(object):
@@ -166,15 +168,16 @@ class WorkspaceApi(object):
                                 'continue.').format(cur_src, extensions))
 
     def export_workspace_dir(self, source_path, target_path, overwrite, headers=None):
-        if os.path.isfile(target_path):
+        os_compatible_target_path = re.sub(LOCAL_OS_COMPATIBLE_PATH_REGEX, '_', target_path)
+        if os.path.isfile(os_compatible_target_path):
             click.echo('{} exists as a file. Skipping this subtree {}'
-                       .format(target_path, source_path))
+                       .format(os_compatible_target_path, source_path))
             return
-        if not os.path.isdir(target_path):
-            os.makedirs(target_path)
+        if not os.path.isdir(os_compatible_target_path):
+            os.makedirs(os_compatible_target_path)
         for obj in self.list_objects(source_path, headers=headers):
             cur_src = obj.path
-            cur_dst = os.path.join(target_path, obj.basename)
+            cur_dst = os.path.join(os_compatible_target_path, obj.basename)
             if obj.is_dir:
                 self.export_workspace_dir(cur_src, cur_dst, overwrite, headers=headers)
             elif obj.is_notebook:
