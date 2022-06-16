3b4012e8e9df24f3fdca6abc69e9cb663be7ab10
Jerry Liang <jerry.liang@databricks.com>
2022-06-16 18:51:58 +0000

    spelling

--- databricks_cli/runs/cli.py
+++ databricks_cli/runs/cli.py
@@ -44,7 +44,7 @@ from databricks_cli.version import print_version_callback, version as cli_versio
 @click.option('--json', default=None, type=JsonClickType(),
               help=JsonClickType.help('/api/2.*/jobs/runs/submit'))
 @click.option('--wait', is_flag=True, default=False,
-              help='If specified, wait for the submitted run to complete.')
+              help='If specified, waits for the submitted run to complete.')
 @api_version_option
 @debug_option
 @profile_option
