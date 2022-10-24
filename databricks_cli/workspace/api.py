1c444a72d0fa97072dfc405f681c3fd4da075e1e
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2022-10-24 16:15:46 +0200

    Don't hide error in WorkspaceApi.import_workspace_dir (#573)

--- databricks_cli/workspace/api.py
+++ databricks_cli/workspace/api.py
@@ -25,7 +25,6 @@ import os
 from base64 import b64encode, b64decode
 
 import click
-from requests.exceptions import HTTPError
 
 from databricks_cli.dbfs.exceptions import LocalFileExistsException
 from databricks_cli.sdk import WorkspaceService
@@ -140,11 +139,7 @@ class WorkspaceApi(object):
         if exclude_hidden_files:
             # for now, just exclude hidden files or directories based on starting '.'
             filenames = [f for f in filenames if not f.startswith('.')]
-        try:
-            self.mkdirs(target_path, headers=headers)
-        except HTTPError as e:
-            click.echo(e.response.json())
-            return
+        self.mkdirs(target_path, headers=headers)
         for filename in filenames:
             cur_src = os.path.join(source_path, filename)
             # don't use os.path.join here since it will set \ on Windows
