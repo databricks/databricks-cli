76c772c811744bc91998a0ad5837938e04db170b
osuleiman <34806809+osuleiman@users.noreply.github.com>
2023-05-17 12:37:14 +0300

    Update cli.py

--- databricks_cli/workspace/cli.py
+++ databricks_cli/workspace/cli.py
@@ -83,7 +83,8 @@ def mkdirs_cli(api_client, workspace_path):
                short_help='Imports a file from local to the Databricks workspace.')
 @click.argument('source_path')
 @click.argument('target_path')
-@click.option('--language', '-l', required=True, type=LanguageClickType(),
+@click.option('--
+              ', '-l', required=False, type=LanguageClickType(),
               help=', '.join(WorkspaceLanguage.ALL))
 @click.option('--format', '-f', default=WorkspaceFormat.SOURCE, type=FormatClickType())
 @click.option('--overwrite', '-o', is_flag=True, default=False)
@@ -91,7 +92,7 @@ def mkdirs_cli(api_client, workspace_path):
 @profile_option
 @eat_exceptions
 @provide_api_client
-def import_workspace_cli(api_client, source_path, target_path, language, format, overwrite): # NOQA
+def import_workspace_cli(api_client, source_path, target_path, language=None, format, overwrite): # NOQA
     """
     Imports a file from local to the Databricks workspace.
 
