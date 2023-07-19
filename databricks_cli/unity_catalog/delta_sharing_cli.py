f01d58da604a5c7bc011c910f6df55492a5dc29a
Jin Zhang <jin.zhang@databricks.com>
2023-07-19 15:09:26 -0700

    update lint
    Signed-off-by: Jin Zhang <jin.zhang@databricks.com>

--- databricks_cli/unity_catalog/delta_sharing_cli.py
+++ databricks_cli/unity_catalog/delta_sharing_cli.py
@@ -21,7 +21,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# pylint: disable=C0302
+# pylint: disable=too-many-lines
 
 import functools
 from json import loads as json_loads
